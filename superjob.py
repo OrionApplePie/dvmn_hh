45d04f7c118d40cc381ee03e0cd388ec6c52a413
Ten Alexander <orionapplepie@gmail.com>
2020-01-27 18:00:42 +1000

    Добавлена авторизация на superjob по логину и паролю

--- superjob.py
+++ superjob.py
@@ -8,30 +8,29 @@ SUPERJOB_API_URL = "https://api.superjob.ru/2.0/user/current/"
 AUTHORIZATION_URL = "https://www.superjob.ru/authorize/"
 ACCESS_TOKEN_API = "https://api.superjob.ru/2.0/oauth2/access_token/"
 
+AUTH_BY_PASSWORD_URL = "https://api.superjob.ru/2.0/oauth2/password/"
+
 
 def main():
     load_dotenv()
 
     secret_key = os.getenv("SUPERJOB_SECRET_KEY")
     app_id = os.getenv("APP_ID")
+    login = os.getenv("LOGIN")
+    passwd = os.getenv("PASSWORD")
 
-    headers = {"X-Api-App-Id": secret_key}
-    response = requests.get(
-        url=AUTHORIZATION_URL,
-        headers=headers,
-        params={"client_id": app_id, "redirect_uri": "localhost/"},
-    )
-    # token_resp = requests.get(
-    #     url=ACCESS_TOKEN_API
-    # )
+    headers = {"X-Api-App-Id": secret_key, "X-User-Type": "reg_user"}
 
-    print(response.text)
-    # print(token_resp.json)
-    # token = requests.get(
-    #     url=ACCESS_TOKEN_API,
-    #     headers=headers,
-    #     param
-    # )
+    params = {
+        "login": login,
+        "password": passwd,
+        "client_id": app_id,
+        "client_secret": secret_key,
+        "hr": 0,
+    }
+
+    resp = requests.get(url=AUTH_BY_PASSWORD_URL, params=params)
+    print(resp.text)
 
 
 if __name__ == "__main__":
