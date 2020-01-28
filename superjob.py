dc599fb1d154456753c5257bd780559824bbd94c
Ten Alexander <orionapplepie@gmail.com>
2020-01-28 19:52:49 +1000

    Улучшено получение вакансий

--- superjob.py
+++ superjob.py
@@ -1,13 +1,13 @@
 import os
+import datetime
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import requests
-
-load_dotenv()
 
 
 AUTH_BY_PASSWORD_URL = "https://api.superjob.ru/2.0/oauth2/password/"
 VACANCIES_SEARCH_URL = "https://api.superjob.ru/2.0/vacancies/"
+MOSKOW_CITY_ID = 4
 
 
 def main():
@@ -30,16 +30,30 @@ def main():
     resp = resp.json()
 
     access_token = resp["access_token"]
-    print(access_token[3:])
+    print(access_token)
 
     headers = {
         "X-Api-App-Id": secret_key,
-        # "Authorization": "Bearer " + access_token[3:]
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Bearer " + access_token[3:],
     }
+    month_ago_date = datetime.datetime.now() - datetime.timedelta(days=30)
 
-    vacancies = requests.get(url=VACANCIES_SEARCH_URL, headers=headers)
-    print(vacancies.json())
+    params_vac = {
+        "town": MOSKOW_CITY_ID,
+        "date_published_from": month_ago_date,
+        "keyword": "программист",
+        "page": 1,
+        "count": 100,
+    }
+    vacancies = requests.get(
+        url=VACANCIES_SEARCH_URL, headers=headers, params=params_vac
+    )
+    res = vacancies.json()
+    print(res)
+    print(res["objects"])
 
 
 if __name__ == "__main__":
+    load_dotenv(find_dotenv())
     main()
