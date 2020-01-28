9364ac3930f81141263b9ad585f4c67b17e21dc5
Ten Alexander <orionapplepie@gmail.com>
2020-01-28 20:03:53 +1000

    Сделано получение и вывод вакансий

--- superjob.py
+++ superjob.py
@@ -11,8 +11,6 @@ MOSKOW_CITY_ID = 4
 
 
 def main():
-    # load_dotenv()
-
     secret_key = os.getenv("SUPERJOB_SECRET_KEY")
     app_id = os.getenv("APP_ID")
     login = os.getenv("LOGIN")
@@ -28,7 +26,6 @@ def main():
 
     resp = requests.get(url=AUTH_BY_PASSWORD_URL, params=params)
     resp = resp.json()
-
     access_token = resp["access_token"]
     print(access_token)
 
@@ -43,15 +40,18 @@ def main():
         "town": MOSKOW_CITY_ID,
         "date_published_from": month_ago_date,
         "keyword": "программист",
-        "page": 1,
-        "count": 100,
+        # "page": 1,
+        # "count": 100,
     }
     vacancies = requests.get(
         url=VACANCIES_SEARCH_URL, headers=headers, params=params_vac
     )
     res = vacancies.json()
-    print(res)
-    print(res["objects"])
+
+    print(len(res["objects"]))
+
+    for item in res["objects"]:
+        print("--> {0}, {1}".format(item["id"], item["profession"]))
 
 
 if __name__ == "__main__":
